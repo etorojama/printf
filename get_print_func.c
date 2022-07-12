3643cd8d2bf17252fafb13729563e7bd89975057
etorojama <etorojama@yahoo.com>
2022-07-12 03:23:44 -0700

    get print func

---
+++ get_print_func.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * get_print_func - returns pointer to function.
+ * @c: character to be used to find pointer function.
+ *
+ * Return: 0 on success
+ */
+
+int (*get_print_func(char c))(va_list, int)
+{
+	forms specifics[] = {
+		{'c', print_ch},
+		{'s', print_str},
+		{'d', print_int},
+		{'i', print_int},
+		{'u', print_unsignd},
+		{'o', print_octal},
+		{'x', print_hex},
+		{'X', print_heX},
+		{'R', print_rot13},
+		{'b', print_b},
+		{'S', print_Str}
+	};
+
+	int i;
+
+	for (i = 0; specifics[i].spec; i++)
+	{
+		if (c == specifics[i].spec)
+			return (specifics[i].fspec);
+	}
+
+	return (NULL);
+}
